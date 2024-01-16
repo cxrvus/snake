c3002836bcdb8827ec0ea6f1d928d4d8473f7e05
Benito Rabe <benito.m.rabe@gmail.com>
2024-01-16 20:27:32 +0100

    major progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use bevy::{prelude::*, window::WindowResolution, transform::commands, render::color};
+use std::{time::Duration, default};
+
+use bevy::{prelude::*, window::WindowResolution, transform::commands, render::color, math};
 use cfg::*;
 
 mod cfg {
-	pub const SIZE: u32 = 16;
-	pub const MID: u32 = SIZE / 2;
-	pub const AREA: u32 = SIZE * SIZE;
-	pub const TILEPX: f32 = 32.;
+	pub mod board {
+		pub const SIZE: u32 = 16;
+		pub const MID: u32 = SIZE / 2;
+		pub const AREA: u32 = SIZE * SIZE;
+		pub const TILEPX: f32 = 32.;
+	}
+
+	pub mod snake {
+		pub const SPEED: f32 = 3.;
+		pub const LENGTH: u32 = 5;
+	}
 }
 
 
@@ -15,42 +24,89 @@ fn main() {
 	.add_plugins(DefaultPlugins
 		.set(WindowPlugin {
 			primary_window: Some(Window {
-				title: "SNAKE :)".into(),
+				title: "$NAKE >:)".into(),
 				resolution: WindowResolution::new(1080., 720.),
 				resizable: false,
 				..default()
 			}),
 			..default()
 		})
-		.set(ImagePlugin::default_nearest())
 	)
+	.init_resource::<Score>()
+	.init_resource::<Length>()
+	.init_resource::<StepTimer>()
+	.init_resource::<Direction>()
 	.add_systems(Startup, (
+		setup_res,
 		setup_camera,
 		setup_board
+	))
+	.add_systems(Update, (
+		set_colors,
+		step
 	))
 	.run();
 }
 
 
 #[derive(Component)]
-struct Tile {
-	kind: Kind,
-	x: u32,
-	y: u32
+struct Tile { kind: Kind, pos: Position }
+
+enum Kind { Empty, Obstacle, Food, Snake(u32) }
+
+#[derive(PartialEq)]
+struct Position { x: u32, y: u32 }
+
+
+#[derive(Resource, Default)]
+struct Score(u32);
+
+#[derive(Resource, Default)]
+struct Length(u32);
+
+
+#[derive(Resource, Default)]
+struct StepTimer(Timer);
+
+impl StepTimer {
+	fn new(speed: f32) -> Self {
+		Self(Timer::new(Duration::from_secs_f32(1./speed), TimerMode::Repeating))
+	}
 }
 
-enum Kind {
-	Empty,
-	Obstacle,
-	Food,
-	Snake(u32)
+
+#[derive(Resource, Default)]
+enum Direction {
+	#[default]
+	Right,
+	Left, Up, Down
+}
+
+impl Direction {
+	fn to_xy(&self) -> (i32, i32) {
+		match self {
+			Direction::Right => (1, 0),
+			Direction::Left => (-1, 0),
+			Direction::Up => (0, 1),
+			Direction::Down => (0, -1),
+		}
+	}
+}
+
+
+fn setup_res (
+	mut length: ResMut<Length>,
+	mut timer: ResMut<StepTimer>
+) {
+	length.0 = snake::LENGTH;
+	*timer = StepTimer::new(snake::SPEED);
 }
 
 fn setup_camera
 (
 	mut commands: Commands
 ) {
-	const CAM_OFFSET: f32 = (MID as f32) * TILEPX;
+	const CAM_OFFSET: f32 = (board::MID as f32) * board::TILEPX;
 	commands.spawn(Camera2dBundle {
 		transform: Transform::from_translation(Vec3::new(CAM_OFFSET, CAM_OFFSET, 0.)),
 		..default()
@@ -61,30 +117,29 @@ fn setup_board
 (
 	mut commands: Commands
 ) {
-	const MAX: u32 = SIZE - 1;
-	const MID: u32 = SIZE / 2;
+	const MAX: u32 = board::SIZE - 1;
+	const MID: u32 = board::SIZE / 2;
 	let (start, end) = (MID-2, MID+2);
 	let range = start..=end;
 
-	let mut board: Vec<Tile> = Vec::with_capacity(AREA as usize);
+	let mut board: Vec<Tile> = Vec::with_capacity(board::AREA as usize);
 
-	for i in 0..AREA {
-		let (x, y) = (i%SIZE, i/SIZE);
+	for i in 0..board::AREA {
+		let (x, y) = (i%board::SIZE, i/board::SIZE);
 		let kind = match (x, y) {
 			(0, _) | (MAX, _) | (_, 0) | (_, MAX) => Kind::Obstacle,
 			(x, MID) if range.contains(&x) => Kind::Snake(x-start),
 			_ => Kind::Empty
 		};
-		board.push(Tile { kind , x , y });
+		board.push(Tile { kind, pos: Position { x, y } });
 	}
 
 	for tile in board {
-		let (x, y) = ((tile.x as f32) * TILEPX, (tile.y as f32) * TILEPX);
+		let (x, y) = ((tile.pos.x as f32) * board::TILEPX, (tile.pos.y as f32) * board::TILEPX);
 
 		commands.spawn(SpriteBundle {
 			sprite: Sprite {
-				color: kind_to_color(&tile.kind),
-				custom_size: Some(Vec2::new(TILEPX, TILEPX)),
+				custom_size: Some(Vec2::new(board::TILEPX, board::TILEPX)),
 				..default()
 			},
 			transform: Transform::from_translation(Vec3::new(x, y, 0.)),
@@ -95,13 +150,71 @@ fn setup_board
 }
 
 
-
-
-fn kind_to_color(kind: &Kind) -> Color {
-	match kind {
-		Kind::Empty => Color::DARK_GRAY,
-		Kind::Obstacle => Color::GRAY,
-		Kind::Food => Color::MIDNIGHT_BLUE,
-		Kind::Snake(_) => Color::WHITE
+fn set_colors
+(
+	mut sprites: Query<(&mut Sprite, &Tile)>
+) {
+	for (mut sprite, tile) in &mut sprites {
+		sprite.color = match tile.kind {
+			Kind::Empty => Color::DARK_GRAY,
+			Kind::Obstacle => Color::GRAY,
+			Kind::Food => Color::MIDNIGHT_BLUE,
+			Kind::Snake(_) => Color::WHITE
+		};
 	}
+}
+
+fn step
+(
+	mut tiles: Query<&mut Tile>,
+	direction: Res<Direction>,
+	mut length: ResMut<Length>,
+	mut score: ResMut<Score>,
+	mut timer: ResMut<StepTimer>,
+	time: Res<Time>
+) {
+	if !timer.0.just_finished() { return; }
+
+	timer.0.tick(time.delta());
+
+	info!("{}", timer.0.elapsed().as_millis());
+
+	let head_tile = tiles.iter()
+		.find(|x| {
+			if let Kind::Snake(i) = x.kind { i == length.0 - 1 } else { false }
+		}).expect("snake head not found")
+	;
+
+	let Position {x, y} = head_tile.pos;
+	let (dx, dy) = direction.to_xy();
+	let next_pos = Position { x: x + dx as u32, y: y + dy as u32 };
+	let mut next_tile = tiles.iter_mut().find(|x| x.pos == next_pos).unwrap();
+
+	match next_tile.kind {
+		Kind::Obstacle | Kind::Snake(_) => { 
+			timer.0.pause();
+			return;
+		}
+		Kind::Food => {
+			next_tile.kind = Kind::Snake(length.0);
+			length.0 += 1;
+			score.0 += 1;
+			return;
+		}
+		Kind::Empty => { }
+	}
+
+	let mut tail_tile = tiles.iter_mut()
+		.find(|x| {
+			if let Kind::Snake(i) = x.kind { i == 0 } else { false }
+		}).expect("snake tail not found")
+	;
+
+	tail_tile.kind = Kind::Empty;
+
+	tiles.iter_mut().for_each(|mut x| {
+		if let Kind::Snake(i) = x.kind {
+			x.kind = Kind::Snake(i-1)
+		}})
+	;
 }
